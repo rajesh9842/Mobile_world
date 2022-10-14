434cc8656a1c997547626e8cecacfbd5134d2332
rajesh9842 <113890832+rajesh9842@users.noreply.github.com>
2022-10-14 09:47:52 +0530

    Update Login.java

--- src/automation/Login.java
+++ src/automation/Login.java
@@ -1,11 +1,11 @@
 package automation;
 
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
 
 
 public class Login {
@@ -16,23 +16,37 @@ public class Login {
 		System.setProperty("webdriver.chrome.driver","C:/Users/rajesh.l/Downloads/chromedriver_win32/chromedriver.exe");
 		 driver = new ChromeDriver();
 		
-		 driver.get("https://mobileworld.banyanpro.com/");
+		 driver.get("https://qualicoach.org/mwapp/index.html");
+		 
 		 driver.findElement(By.cssSelector("button[type='submit']")).click();
+		 
 		 driver.findElement(By.xpath("//input[@id='username']")).sendKeys(username);
+		 
 		 driver.findElement(By.xpath("//input[@id='password']")).sendKeys(password);
+		 
 		 driver.findElement(By.cssSelector("label[for='remember-me']")).click();
 		 Thread.sleep(1000);
 		 driver.findElement(By.xpath("//a[@type='submit']")).click();
-		 Thread.sleep(1000);
-		 int L= username.length();
-	       if ((L < 3) || (L > 20)) {
-	            Assert.assertEquals("https://mobileworld.banyanpro.com/sign.html", driver.getCurrentUrl());
-	        } else {
-	            Assert.assertEquals("https://mobileworld.banyanpro.com/index.html", driver.getCurrentUrl());
-	        }
 		 
+		 int L= username.length();
+		 int Length=password.length();
+	       if ((L < 3) || (L > 20)) {
+	    	   AssertJUnit.assertEquals("https://qualicoach.org/mwapp/sign.html", driver.getCurrentUrl());
+	        } else {
+	            AssertJUnit.assertEquals("https://qualicoach.org/mwapp/index.html", driver.getCurrentUrl());
+	        }
+	       
+	       if ((Length < 3) || (Length > 10)) {
+	    	   AssertJUnit.assertEquals("https://qualicoach.org/mwapp/sign.html", driver.getCurrentUrl());
+	        } else {
+	            AssertJUnit.assertEquals("https://qualicoach.org/mwapp/index.html", driver.getCurrentUrl());
+	        }
+	    	   
+	       
+	       Thread.sleep(1000);
 		 	
-		 driver.quit();
+		 driver.close();
+		 
 		
 		 
 		
